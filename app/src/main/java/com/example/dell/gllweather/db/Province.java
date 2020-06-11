77f66754d11911178b8ed2ac612277e0c08a58d1
gll2020 <gllweather@163.com>
2020-06-11 23:32:23 +0800

    First commit.

---
+++ app/src/main/java/com/example/dell/gllweather/db/Province.java
@@ -0,0 +1,32 @@
+package com.example.dell.gllweather.db;
+
+//extends DataSupport
+
+
+/**
+ * Created by dell on 2020/6/1.
+ */
+
+public class Province{
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceName=provinceName;
+    }
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode=provinceCode;
+    }
+}
